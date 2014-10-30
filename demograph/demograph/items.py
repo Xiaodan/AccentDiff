ea73258c7862fdb5a4b79c5d613b40c8eb38161a
Xiaodan Zhang <zhang349@illinois.edu>
2014-10-30 03:27:06 -0500

    Modified parse function and Item class so that we can output bio_info in JSON format

--- demograph/demograph/items.py
+++ demograph/demograph/items.py
@@ -11,6 +11,14 @@ import scrapy
 class DemographItem(scrapy.Item):
     # define the fields for your item here like:
     
-    brand_name = scrapy.Field()
-    link = scrapy.Field()
+    birth_place = scrapy.Field()
+    native_language = scrapy.Field()
+    other_language = scrapy.Field()
+    age = scrapy.Field()
+    gender = scrapy.Field()
+    age_of_english_onset = scrapy.Field()
+    enlish_learning_method = scrapy.Field()
+    english_residence = scrapy.Field()
+    length_of_english_residence = scrapy.Field()
+    
     pass
--- demograph/demograph/spiders/demograph_spider.py
+++ demograph/demograph/spiders/demograph_spider.py
@@ -1,30 +1,45 @@
 import scrapy
-import string
+import json
 from demograph.items import DemographItem
 
 class DemoSpider(scrapy.Spider):
     name = "demo"
-    allowed_domains = ["pronto.com"]
-    base_url = "http://www.pronto.com/brands/"
-    # There are 27 pages we need to crawl in total.
+    allowed_domains = ["accent.gmu.edu"]
+    base_url = "http://accent.gmu.edu/browse_language.php?function=detail&speakerid="
     start_urls = []
-    start_urls.append(base_url + "num")
-    a_to_z = string.lowercase
-    for each_char in a_to_z:
-        start_urls.append(base_url + each_char)
+    start_urls.append(base_url+"239")
+    
 
 
     def parse(self, response):
-        brand_name_dict = {}
-        # The div classes we use are based on the website source. 
-        for sel in response.xpath('//div[@class="brandStoresAlphaList clearfix"]/ul/li'):
-            item = DemographItem()
-            item['brand_name'] = sel.xpath('a/text()').extract()
-            item['link'] = sel.xpath('a/@href').extract()
-            yield item
 
-            # Output a file containing the original data.
-            with open('valid_brand_list.txt', 'a') as f:
-                f.write('{0}\n'.format(str(item['brand_name'][0])))
+        item = DemographItem()
+        result = response.xpath('//ul[@class="bio"]/li')
+        bio_data = result.xpath('em/following-sibling::text()').extract()
+        for i in range(0, len(bio_data)):
+            bio_data[i] = str(bio_data[i]).strip()
+        links = result.xpath('a/text()').extract()
+        for i in range(0, len(links)):
+            links[i] = str(links[i]).strip()
+        # print bio_data
+        # print links
+        item['birth_place'] = bio_data[0]
+        item['native_language'] = links[1]
+        item['other_language'] = bio_data[3]
+        age_gender = bio_data[4].split(", ")
+        item['age'] = age_gender[0]
+        item['gender'] = age_gender[1]
+        item['age_of_english_onset'] = bio_data[5]
+        item['enlish_learning_method'] = bio_data[6]
+        item['english_residence'] = bio_data[7]
+        item['length_of_english_residence'] = bio_data[8] 
+
+        # print item
+        yield item
+       
+
+        # Output a file containing the original data.
+        # with open('../DATA/biographies.txt', 'a') as f:
+        #    f.write(item)
         
 

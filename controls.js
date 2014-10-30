40a2fef3c11eadbe3e9a0dc3562ab35df20d8863
Shubhanshu Mishra <shubhanshumishra@gmail.com>
2014-10-30 09:37:02 -0500

    Build the working page

---
+++ controls.js
@@ -0,0 +1,16 @@
+$(".chosen-select").chosen().change(function(e){
+	console.log(e.target.value);
+	curr_lang = e.target.value;
+	console.log("Next Language is Created on Choosen", curr_lang);
+});
+
+$("#update").on("click", function (argument) {
+	// body...
+	var lang1 = $("#lang1").val();
+	var lang2 = $("#lang2").val();
+	console.log(lang1, lang2);
+	$(".waveform")[0].innerHTML = "";
+	$(".container").remove();
+	drawWaveForm("./DATA/JSON/"+lang1+".json", "Native "+lang1.toUpperCase());
+	drawWaveForm("./DATA/JSON/"+lang2+".json", "Native "+lang2.toUpperCase(), "base");
+});
--- viz.js
+++ viz.js
@@ -1,27 +1,59 @@
-d3.json("DATA/JSON/tamil2.json", function (error, data) {
-	// body...
-	var waveform = WaveformData.create(data);
-	wf = waveform;
-	console.log(waveform.duration);
-	var layout = d3.select("body").append("svg");
+var layout = d3.select("body").append("svg");
 	layout.attr("width", 800)
-		.attr("height", 600);
-	var graph = layout.append("g");
-	var x = d3.scale.linear();
-	var y = d3.scale.linear();
-	var offsetX = 100;
+		.attr("height", 300)
+		.classed("waveform", true);
 
-	x.domain([0, waveform.adapter.length]).rangeRound([0, 1024]);
-	y.domain([d3.min(waveform.min), d3.max(waveform.max)]).rangeRound([offsetX, -offsetX]);
+/*var layout = container
+			.append("svg")
+			.attr("width", 800)
+			.attr("height", 300);*/
 
-	var area = d3.svg.area()
-	  .x(function(d, i){ return x(i) })
-	  .y0(function(d, i){ return y(waveform.min[i]) })
-	  .y1(function(d, i){ return y(d) });
+function drawWaveForm(datafile, name, cssClass){
+	d3.json(datafile, function (error, data) {
+	// body...
+		var filename = datafile.replace(/^.*\/|\.[^.]*$/g, '');
+		console.log(filename);
 
-	graph.append("path")
-	  .datum(waveform.max)
-	  .attr("transform", function(){ return "translate(0, "+offsetX+")"; })
-	  .attr("class", "area")
-	  .attr("d", area);
-});
+		if(cssClass === undefined){
+			cssClass = "";
+		}
+		var waveform = WaveformData.create(data);
+		console.log(waveform.duration);
+		var container = d3.select("body")
+			.append("div")
+			.classed("container", true);
+		container.append("h3")
+			.text(name+": "+cssClass)
+
+
+		var graph = layout.append("g")
+			//.attr("transform", function(){ return "translate(0, "+nextY+")"; });
+		var x = d3.scale.linear();
+		var y = d3.scale.linear();
+		var offsetX = 100;
+		//nextY += 3*offsetX;
+
+		x.domain([0, waveform.adapter.length]).rangeRound([0, 1024]);
+		y.domain([-d3.max(waveform.max), d3.max(waveform.max)]).rangeRound([offsetX, -offsetX]);
+
+		var area = d3.svg.area()
+		  .x(function(d, i){ return x(i) })
+		  .y0(function(d, i){ return y(waveform.min[i]) })
+		  .y1(function(d, i){ return y(d) });
+
+
+		graph.append("path")
+		  .datum(waveform.max)
+		  .attr("transform", function(){ return "translate(0, "+offsetX+")"; })
+		  .classed("area", true)
+		  .classed(cssClass, true)
+		  .attr("d", area);
+		var audioElm = container.append("audio")
+			.attr("controls", "controls");
+		audioElm.append("source")
+			.attr("src", "./DATA/MP3/"+filename+".mp3");
+	});
+}
+
+drawWaveForm("./DATA/JSON/tamil2.json", "Native Tamil");
+drawWaveForm("./DATA/JSON/english2.json", "Native English", "base");
